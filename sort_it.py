737af8643415ab1cf9440a6c6aad01d02d99108b
persona1346 <bosoeva2001@mail.ru>
2020-01-10 16:48:20 +0300

    game in pygame

---
+++ sort_it.py
@@ -0,0 +1,292 @@
+import pygame
+import sys
+import random
+
+pygame.init()
+pygame.font.init()
+
+screen = pygame.display.set_mode((480,800))
+pygame.display.set_caption("Sort it Game")
+clock = pygame.time.Clock()
+
+background_image = pygame.image.load('images/background.jpg')
+bottom_image = pygame.image.load('images/bottom.jpg')
+rules_image = pygame.image.load('images/rules.jpg')
+pause_image = pygame.image.load('images/pause.png')
+
+
+tanks = [pygame.image.load('images/tank_1.png'), pygame.image.load('images/tank_2.png'),
+         pygame.image.load('images/tank_3.png'), pygame.image.load('images/tank_4.png')]
+
+trashes = [pygame.image.load('images/trash_1.png'), pygame.image.load('images/trash_2.png'),
+         pygame.image.load('images/trash_3.png'), pygame.image.load('images/trash_4.png')]
+
+mascot = pygame.image.load('images/mascot.png')
+mascot = pygame.transform.scale(mascot, (mascot.get_width()//7,
+                                         mascot.get_height()//7))
+
+lifes = [pygame.image.load('images/life_4.png'),pygame.image.load('images/life_3.png'),
+         pygame.image.load('images/life_2.png'),pygame.image.load('images/life_1.png')]
+
+for n in range(4):
+    lifes[n] = pygame.transform.scale(lifes[n], (lifes[n].get_width()//16,
+                                                lifes[n].get_height()//16))
+
+font = pygame.font.Font('fonts/font_1.ttf', 42)
+
+sound_true = pygame.mixer.Sound('sounds/true_tank.wav')
+sound_false = pygame.mixer.Sound('sounds/mistake.wav')
+sound_gameover = pygame.mixer.Sound('sounds/game_over.wav')
+sound_button = pygame.mixer.Sound('sounds/button.wav')
+
+x = random.randint(0, 450)
+y = 0
+speed = 7
+j = random.randint(0, 3)
+score = 0
+count_try = 0
+count_play = 0
+
+
+class Menu:
+    def __init__(self, punkts = []):
+        self.punkts = punkts
+
+    def render(self, screen, font, num_punkt):
+        for i in self.punkts:
+            if num_punkt == i[5]:
+                screen.blit(font.render(i[2], 1, i[4]), (i[0], i[1]))
+            else:
+                screen.blit(font.render(i[2], 1, i[3]), (i[0], i[1]))
+
+    def menu(self):
+
+        if punkts[0][2] == 'Game':
+            pygame.mixer.music.load('sounds/menu_music.mp3')
+            pygame.mixer.music.play(-1)
+            pygame.mixer.music.set_volume(0.5)
+        elif punkts[0][2] == 'Resume':
+            pygame.mixer_music.pause()
+        done = True
+        # pygame.mouse.set_visible(True)
+        # pygame.key.set_repeat(0,0)
+        punkt = 0
+        while done:
+            screen.blit(background_image, (0, 0))
+
+            mp = pygame.mouse.get_pos()
+            for i in self.punkts:
+                if mp[0]>i[0] and mp[0]<i[0]+155 and mp[1]>i[1] and mp[1]<i[1]+80:
+                    punkt = i[5]
+
+            self.render(screen, font, punkt)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    #sound_button.play()
+                    if event.key == pygame.K_RETURN:
+                        if punkt == 0:
+                            done = False
+                        if punkt == 1:
+                            menu_rules()
+                        if punkt == 2:
+                            sys.exit()
+                    if event.key == pygame.K_ESCAPE:
+                        sys.exit()
+                    if event.key == pygame.K_UP:
+                        if punkt > 0:
+                            punkt -= 1
+                    if event.key == pygame.K_DOWN:
+                        if punkt < len(self.punkts)-1:
+                            punkt += 1
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    #sound_button.play()
+
+                    if punkt == 0:
+                        done = False
+                    if punkt == 1:
+                        menu_rules()
+                    if punkt == 2:
+                        sys.exit()
+
+            screen.blit(screen, (0, 30))
+
+            pygame.display.update()
+
+
+def menu_rules():
+    done = True
+    back_button = 0
+    while done:
+        screen.blit(rules_image, (0, 0))
+        screen.blit(font.render("Back", 1, (250, 250, 30)), (160, 600))
+
+        mp = pygame.mouse.get_pos()
+
+        if mp[0]>180 and mp[0]<300 and mp[1]>600 and mp[1]<660:
+            screen.blit(font.render("Back", 1, (250, 30, 250)), (160, 600))
+            back_button = 1
+
+        for event in pygame.event.get():
+            #sound_button.play()
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    done = False
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if back_button == 1:
+                    done = False
+
+        pygame.display.update()
+
+
+def draw_tanks():
+    i = 0
+    for tank in tanks:
+        scale = pygame.transform.scale(tank, ((tank.get_width()*3)//4,
+                                              (tank.get_height()*3)//4))
+        screen.blit(scale, (i, 650))
+        i += 120
+
+
+def draw_score():
+    font = pygame.font.Font('fonts/font_1.ttf', 25)
+    text = font.render("Score: " + str(score), True, [0, 0, 0])
+    screen.blit(text, [30, 30])
+
+
+def draw_mascot(q, p=650, up=1, mascot_isRun: bool = True):
+    while mascot_isRun:
+        screen.blit(background_image, (0, 0))
+        screen.blit(mascot, ((q // 120) * 120, p))
+        if up:
+            p -= 5
+        else:
+            p += 5
+
+        if p <= 570:
+            up = 0
+        if p >= 660:
+            y = 0
+            x = random.randint(0, 450)
+            j = random.randint(0, 3)
+            p = 650
+            up = 1
+            mascot_isRun = False
+
+        draw_tanks()
+        draw_score()
+        screen.blit(lifes[count_try],(20,60))
+        screen.blit(pause_image,(400,30))
+
+        pygame.display.update()
+        pygame.time.delay(100)
+
+
+punkts = [(174, 140, u'Game', (250, 250, 30), (250, 30, 250), 0),
+          (172, 210, u'Rules', (250, 250, 30), (250, 30, 250), 1),
+          (186, 280, u'Quit', (250, 250, 30), (250, 30, 250), 2)]
+
+game = Menu(punkts)
+game.menu()
+
+pygame.mixer.music.load('sounds/fon_music.mp3')
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.2)
+
+run = True
+while run:
+    screen.blit(background_image, (0, 0))
+
+    if y < 650:
+        screen.blit(trashes[j], (x, y))
+
+    pygame.time.delay(100)
+    mp = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run= False
+        if mp[0] > 400 and mp[0] <450 and mp[1] > 30 and mp[1] < 80 and event.type == pygame.MOUSEBUTTONDOWN:
+            punkts = [(150, 140, u'Resume', (250, 250, 30), (250, 30, 250), 0),
+                      (172, 210, u'Rules', (250, 250, 30), (250, 30, 250), 1),
+                      (186, 280, u'Quit', (250, 250, 30), (250, 30, 250), 2)]
+
+            game_pause = Menu(punkts)
+            game_pause.menu()
+
+            pygame.mixer.music.unpause()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                punkts = [(150, 140, u'Resume', (250, 250, 30), (250, 30, 250), 0),
+                          (172, 210, u'Rules', (250, 250, 30), (250, 30, 250), 1),
+                          (186, 280, u'Quit', (250, 250, 30), (250, 30, 250), 2)]
+
+                game_pause = Menu(punkts)
+                game_pause.menu()
+
+                pygame.mixer.music.unpause()
+
+                # pygame.key.set_repeat(1,1)
+                # pygame.mouse.set_visible(False)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= speed*2
+    if keys[pygame.K_RIGHT] and x < 450:
+        x += speed*2
+    if keys[pygame.K_DOWN]:
+        y += speed*2
+
+    fl = False
+    if y < 650:
+        y += speed
+    else:
+        if j == 0 and (x + trashes[j].get_width()/2) < tanks[0].get_width()*3//4 :
+            fl = True
+        elif j == 1 and 120 < (x + trashes[j].get_width()/2) < tanks[1].get_width()*3//4 + 120:
+            fl = True
+        elif j == 2 and 240 < (x + trashes[j].get_width()/2) < tanks[2].get_width()*3//4 + 240:
+            fl = True
+        elif j == 3 and 360 < (x + trashes[j].get_width()/2) < tanks[0].get_width()*3//4 + 360:
+            fl = True
+
+        if fl == True:
+            sound_true.play()
+            score += 1
+            draw_mascot(x)
+        else:
+            if score > 0:
+                score -= 1
+            count_try +=1
+            if count_play == 0:
+                sound_false.play()
+
+        y = 0
+        x = random.randint(0, 450)
+        j = random.randint(0, 3)
+
+    if count_try > 3:
+        text = font.render("GAME OVER", True, [250, 0, 0])
+        screen.blit(text, [110, 350])
+        pygame.mixer.music.pause()
+        if count_play == 0:
+            sound_gameover.play()
+            count_play = 1
+        x = 1000
+        y = 1000
+    else:
+        screen.blit(lifes[count_try],(20,60))
+
+    screen.blit(bottom_image, (0, 655))
+
+    draw_tanks()
+    draw_score()
+    screen.blit(pause_image,(400,30))
+
+    pygame.display.update()
+        # clock.tick(60)
+
+pygame.quit()
